91d44409123d205620a22ee15b19110632fdbe1c
jms-guy <guyjamie92@gmail.com>
2025-12-03 10:58:27 -0500

    buffering

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	messages := make([]byte, 8)
+
+	for {
+		numBytes, err := f.Read(messages)
+		if numBytes > 0 {
+			fmt.Printf("read: %s\n", string(messages)[:numBytes])
+		}
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			} else {
+				log.Fatal(err)
+			}
+		}
+	}
 }
