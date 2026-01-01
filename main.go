95bde905dc56d698eb2b917e61e0cb02d4bc71d9
jms-guy <guyjamie92@gmail.com>
2026-01-01 14:54:29 -0500

    tcp listening

--- main.go
+++ main.go
@@ -4,28 +4,64 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
+	"strings"
 )
 
 func main() {
-	f, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	messages := make([]byte, 8)
+	defer listener.Close()
 
 	for {
-		numBytes, err := f.Read(messages)
-		if numBytes > 0 {
-			fmt.Printf("read: %s\n", string(messages)[:numBytes])
-		}
+		conn, err := listener.Accept()
 		if err != nil {
-			if err == io.EOF {
-				os.Exit(0)
-			} else {
-				log.Fatal(err)
+			log.Fatal(err)
+		}
+		fmt.Println("Connection accepted")
+
+		messages := getLinesChannel(conn)
+
+		for m := range messages {
+			fmt.Println(m)
+		}
+		fmt.Println("Connection closed")
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	messChannel := make(chan string)
+
+	go func() {
+		defer close(messChannel)
+		messages := make([]byte, 8)
+		currLine := ""
+
+		for {
+			clear(messages)
+			numBytes, err := f.Read(messages)
+			if err != nil {
+				if err == io.EOF {
+					return
+				} else {
+					log.Fatal(err)
+				}
+			}
+			if numBytes > 0 {
+				parts := strings.Split(string(messages), "\n")
+				currLine += parts[0]
+				if len(parts) > 1 {
+					messChannel <- currLine
+					currLine = parts[1]
+				}
+			}
+			if numBytes < 8 {
+				messChannel <- currLine
 			}
 		}
-	}
+	}()
+
+	return messChannel
 }
